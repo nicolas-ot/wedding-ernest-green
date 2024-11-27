f2c1fd3a79fd1ffde507726e9b25537c664a6431
Nicolas <nicolastheodarus@gmail.com>
2024-11-27 10:53:10 +0100

    setup action

--- app/page.js
+++ app/page.js
@@ -7,13 +7,6 @@ import Footer from "../components/Footer";
 export default function Home() {
     return (
         <>
-            <div>
-                <audio id="background-music" autoPlay loop>
-                    <source src="/assets/audio/LostWoods.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                </audio>
-            </div>
-
             <HeaderSlider/>
             <Countdown/>
             <ContactSection/>
