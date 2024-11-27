b9bf7e2a91c2c9386547ead93c3c533d99b5a8c6
Nicolas <nicolastheodarus@gmail.com>
2024-11-27 17:40:59 +0100

    setup action

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/wedding-ernest-green",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    // basePath: "/wedding-ernest-green",
+    // output: "export",  // <=== enables static exports
+    // images: {
+    //     unoptimized: true,
+    // },
 };
 
 
